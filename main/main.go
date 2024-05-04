c8186ecb2fb7e7027156b02f07913d6f370b0b8c
Eduardo Tongson <propolice@gmail.com>
2024-05-04 16:27:05 +0800

    Some env tests.

--- main/main.go
+++ main/main.go
@@ -8,6 +8,13 @@ func main() {
 	p := func(s string) {
 		print(s)
 	}
-	a := gl.RunArg{Dir: "/etc", Exe: "ls", Stdout: p}
+	script := `
+	set -efu
+	VAR="XXX"
+	export EVAR="YYY"
+	sudo --preserve-env=XVAR,VAR -s /bin/dash -c '/usr/bin/env' e
+	`
+	env := []string{"LC_ALL=C", "XVAR=ZZZ"}
+	a := gl.RunArg{Env: env, Dir: "/etc", Exe: "sh", Args: []string{"-c", script}, Stdout: p}
 	_, _ = a.Run()
 }
